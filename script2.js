757d52d1d348df2e294b75c111dc022bc9d1e985
Caesar0303 <116821172+Caesar0303@users.noreply.github.com>
2023-01-12 19:47:02 +0600

    Delete script2.js

--- script2.js
+++
@@ -1,9 +0,0 @@
-
-function drawsnake () {
-    let world = document.getElementById('world2');
-    let snake = document.createElement('div') ;
-    world.appendChild(snake);
-    snake.classList.add('snake2')
-}
-
-drawsnake();
